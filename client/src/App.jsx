9259acd70b879dfa7def8c9588203fed5d354d02
sharadkumar-jsr <sharadkumar.jsr@gmail.com>
2026-04-27 11:49:54 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth }        from './context/AuthContext.jsx';
 import { PlayerProvider } from './context/PlayerContext.jsx';
-import LoginPage            from './pages/LoginPage.jsx';
-import SessionsPage         from './pages/SessionsPage.jsx';
-import NewSessionPage       from './pages/NewSessionPage.jsx';
-import ManualMovementPage   from './pages/ManualMovementPage.jsx';
-import SetupPairsPage       from './pages/SetupPairsPage.jsx';
-import DirectorPage         from './pages/DirectorPage.jsx';
-import LeaderboardPage      from './pages/LeaderboardPage.jsx';
-import PlayJoinPage         from './pages/PlayJoinPage.jsx';
-import PlayerDashboard      from './pages/PlayerDashboard.jsx';
-import PlayerResults        from './pages/PlayerResults.jsx';
-import TravellerPage        from './pages/TravellerPage.jsx';
+import LoginPage              from './pages/LoginPage.jsx';
+import SessionsPage           from './pages/SessionsPage.jsx';
+import NewSessionPage         from './pages/NewSessionPage.jsx';
+import ManualMovementPage     from './pages/ManualMovementPage.jsx';
+import SetupPairsPage         from './pages/SetupPairsPage.jsx';
+import DirectorPage           from './pages/DirectorPage.jsx';
+import LeaderboardPage        from './pages/LeaderboardPage.jsx';
+import ArchivedSessionsPage   from './pages/ArchivedSessionsPage.jsx';
+import DirectorResultsPage    from './pages/DirectorResultsPage.jsx';
+import PlayJoinPage           from './pages/PlayJoinPage.jsx';
+import PlayerDashboard        from './pages/PlayerDashboard.jsx';
+import PlayerResults          from './pages/PlayerResults.jsx';
+import TravellerPage          from './pages/TravellerPage.jsx';
 
 function PrivateRoute({ children }) {
   const { auth } = useAuth();
@@ -25,19 +27,21 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/"      element={<Navigate to="/sessions" replace />} />
 
-        {/* Director */}
-        <Route path="/sessions"                element={<PrivateRoute><SessionsPage /></PrivateRoute>} />
-        <Route path="/sessions/new"            element={<PrivateRoute><NewSessionPage /></PrivateRoute>} />
-        <Route path="/sessions/:id/movement"   element={<PrivateRoute><ManualMovementPage /></PrivateRoute>} />
-        <Route path="/sessions/:id/setup"      element={<PrivateRoute><SetupPairsPage /></PrivateRoute>} />
-        <Route path="/sessions/:id/director"   element={<PrivateRoute><DirectorPage /></PrivateRoute>} />
-        <Route path="/sessions/:id/leaderboard" element={<PrivateRoute><LeaderboardPage /></PrivateRoute>} />
+        {/* Director routes */}
+        <Route path="/sessions"                   element={<PrivateRoute><SessionsPage /></PrivateRoute>} />
+        <Route path="/sessions/new"               element={<PrivateRoute><NewSessionPage /></PrivateRoute>} />
+        <Route path="/sessions/archived"          element={<PrivateRoute><ArchivedSessionsPage /></PrivateRoute>} />
+        <Route path="/sessions/:id/movement"      element={<PrivateRoute><ManualMovementPage /></PrivateRoute>} />
+        <Route path="/sessions/:id/setup"         element={<PrivateRoute><SetupPairsPage /></PrivateRoute>} />
+        <Route path="/sessions/:id/director"      element={<PrivateRoute><DirectorPage /></PrivateRoute>} />
+        <Route path="/sessions/:id/leaderboard"   element={<PrivateRoute><LeaderboardPage /></PrivateRoute>} />
+        <Route path="/sessions/:id/full-results"  element={<PrivateRoute><DirectorResultsPage /></PrivateRoute>} />
 
-        {/* Players */}
-        <Route path="/play/:token"            element={<PlayJoinPage />} />
-        <Route path="/play/:token/score"      element={<PlayerDashboard />} />
-        <Route path="/play/:token/results"    element={<PlayerResults />} />
-        <Route path="/play/:token/traveller"  element={<TravellerPage />} />
+        {/* Player routes */}
+        <Route path="/play/:token"           element={<PlayJoinPage />} />
+        <Route path="/play/:token/score"     element={<PlayerDashboard />} />
+        <Route path="/play/:token/results"   element={<PlayerResults />} />
+        <Route path="/play/:token/traveller" element={<TravellerPage />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
